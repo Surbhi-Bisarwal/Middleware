2e3d000ffc0c244d0b4111125c7a9a7acf0dc8a5
surbhi <surbhibisarwal79@gmail.com>
2025-01-30 13:24:34 +0530

    route(Middleware2)

--- index.js
+++ index.js
@@ -1,34 +1,19 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
-//loading middleware into app
-
+//inbuilt middleware
 app.use(express.json());
 
-//middleware -logging, auth, validation
+
+const route = require('./routes/route')
+//mounting routes
+app.use('/api', route)
+
+  //->  /api/student
+  //--> /api/admin
 
 
-//creation of middleware
-const loggingMiddleware = function (req, res, next) {
-  console.log("Logging kar rah hu");
-  next();
-};
-//loading middleware into application
-app.use(loggingMiddleware);
-//********************** */
-const authMiddleware = function (req, res, next) {
-  console.log("authentication ho rha h ");
-  next();
-};
-app.use(authMiddleware);
-//*********************** */
-const validationMiddleware = function (req, res, next) {
-  console.log("validate ho rha h");
-  next();
-};
-app.use(validationMiddleware);
-//**************************************** */
+
 app.get("/", (req, res) => {
   console.log("main route handler hu");
   console.log(req.body);
